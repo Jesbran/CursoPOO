408e40105b4b29ab8c2ae57b625231880830575a
Jesbran <brandon150hnd@gmail.com>
2022-10-05 15:40:03 -0500

    metodo constructor python

--- Python/car.py
+++ Python/car.py
@@ -1,5 +1,14 @@
+from account import Account
+
+
 class Car:
     id = int
     licence = str
-    driver = str
+    driver = Account('', '')
     passengers = int
+    
+    def __init__(self, licence, driver):
+        #self.id
+        self.licence = licence
+        self.driver = driver
+        #self.passengers
--- Python/main.py
+++ Python/main.py
@@ -1,7 +1,8 @@
 from car import Car
+from account import Account
 
 def run():
-    print("Hola mundo")
+    '''print("Hola mundo")
     car = Car()
     car.driver = "María Cruz"
     car.licence = "VIS460"
@@ -12,7 +13,11 @@ def run():
     car2.driver = "Rocio FLores"
     car2.licence = "BCI498"
     car2.passengers = 4
-    print(vars(car2))
+    print(vars(car2))'''
+    car = Car('FOP87', Account('Maria Cruz', 'PFV97'))
+    print(vars(car))
+    print(vars(car.driver))
+
 
 if __name__ == '__main__':
     run()
